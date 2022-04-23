db1cfeddcc6ac248a41243491bf4513b22faa567
Max Verevkin <maxxverrr@gmail.com>
2022-04-23 19:53:12 +0300

    nvim: toggleterm: use new autocmd api

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup {
   size = 20,
-  open_mapping = [[<c-t>]],
+  open_mapping = "<c-t>",
   hide_numbers = true,
   shade_filetypes = {},
   shade_terminals = true,
@@ -26,16 +26,18 @@ toggleterm.setup {
   },
 }
 
-function _G.set_terminal_keymaps()
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-end
-
-vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  callback = function()
+    local opts = { buffer = true }
+    vim.keymap.set("t", "<C-n>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-n>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+  end,
+})
 
 function _G._toggle_term(opts)
   local Terminal = require("toggleterm.terminal").Terminal
